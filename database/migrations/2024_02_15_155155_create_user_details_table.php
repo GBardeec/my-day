88e08360cc22797e727a0efd06151bc58230a717
GBardeec <ilyas_muhamatdinov@mail.ru>
2024-02-15 20:47:12 +0400

    add modal for finance

--- database/migrations/2024_02_15_155155_create_user_details_table.php
+++ database/migrations/2024_02_15_155155_create_user_details_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_details', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->string('img');
             $table->string('name');
             $table->string('surname');
             $table->string('patronymic');
